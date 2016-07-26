88bc51457f72b2f7b9488573419717dbeb9857b0
chuggafan <chuggafans@gmail.com>
2016-07-25 21:57:30 -0400

    added a pause

--- 1PICALBUMSARECANCER/stuff.bat
+++ 1PICALBUMSARECANCER/stuff.bat
@@ -3,5 +3,5 @@ start ./nuget.exe restore -SolutionDirectory ..\1PICALBUMSARECANCER.sln
 
 start C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBUILD.exe ..\1PICALBUMSARECANCER.sln
 echo goto this directory's bin/DEBUG folder for important stuffs!
-
+pause
 ::This stuff SHOULD work, PLEASE tell me if it doesn't, an EXE file will be in the /bin/DEBUG output::
