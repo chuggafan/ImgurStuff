16855e7a0ea4ecd8b9ad601bd1a5bc096f41877f
chuggafan <chuggafans@gmail.com>
2016-07-26 22:45:59 -0400

    Fix, with testing!

--- 1PICALBUMSARECANCER/Stuff (2).bat
+++ 1PICALBUMSARECANCER/Stuff (2).bat
@@ -1,6 +1,5 @@
-﻿
 @echo off
-./nuget.exe restore -SolutionDirectory ../1PICALBUMSARECANCER.sln
+.\nuget.exe restore -SolutionDirectory ../1PICALBUMSARECANCER.sln
 
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBUILD.exe ..\1PICALBUMSARECANCER.sln
 echo This has important things when you continue, be sure to input the imgur application ID next, and you'll be fine
