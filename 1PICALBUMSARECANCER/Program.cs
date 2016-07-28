adf2897cfc080eb6cfd071c4bdb1a51c6616c702
chuggafan <chuggafans@gmail.com>
2016-07-28 17:02:58 -0400

    Should fix EVERYTHING

--- 1PICALBUMSARECANCER/Program.cs
+++ 1PICALBUMSARECANCER/Program.cs
@@ -11,6 +11,7 @@ namespace _1PICALBUMSARECANCER
     {
         static void Main(string[] args)
         {
+            Console.Title = "Setup";
             Console.WriteLine("Input Client ID, It is also known as an application ID");
             string ID = Console.ReadLine();
             var imgur = new Imgur(new ImgurNet.Authentication.ClientAuthentication(ID, true));
@@ -23,7 +24,7 @@ namespace _1PICALBUMSARECANCER
             reddit.RateLimit = WebAgent.RateLimitMode.Pace;
             Subreddit main = Subreddit.GetRSlashAll(reddit);
             List<string> alreadySeen;
-            if(File.Exists("./seen.txt"))
+            if (File.Exists("./seen.txt"))
             {
                 alreadySeen = File.ReadAllLines("./seen.txt").ToList();
             }
@@ -32,34 +33,44 @@ namespace _1PICALBUMSARECANCER
                 alreadySeen = new List<string>();
             }
             // To add a subreddit to filter from, add it to this list of items, an example would be say, "aww", "The_Donald", for example, I added two to show you the syntax
-            Subreddit[] exclusions = new Subreddit[] { reddit.GetSubreddit("aww"), reddit.GetSubreddit("The_Donald") };
+            Subreddit[] exclusions = new Subreddit[] { };
             while (true)
             {
                 foreach (var post in main.New.Take(100))
                 {
-                    Console.WriteLine("Found " + post.Url + " in " + post.Id + " as a link");
                     Console.Title = "Searching /r/all";
+                    Console.WriteLine("Found " + post.Url + " in " + post.Id + " as a link");
+                    if (post.IsSelfPost || !System.Text.RegularExpressions.Regex.IsMatch(post.Url.Host.ToString(), "imgur"))
+                        goto end;
                     if (alreadySeen.Contains(post.Id))
                         continue;
-                    if ("imugr".Contains(post.Url.ToString()))
+                    Console.WriteLine(post.Id + " is an imgur link and it's link is " + post.Url.ToString());
+                    string album = post.Url.ToString().Substring(post.Url.ToString().LastIndexOf('/'));
+                    retry:;
+                    try
                     {
-                        Console.WriteLine(post.Id +  "is an imgur link and it's link is" + post.Url.ToString());
-                        string album = post.Url.ToString().Substring(post.Url.ToString().LastIndexOf('/'));
-                       
                         if (galleryEndpoint.GetAlbumDetailsAsync(album).Result.Data.ImagesCount == 1)
                         {
                             Console.Title = "Searching imgur to see if it is a 1 post album";
-
-                            if(!exclusions.ToList().Contains(post.Subreddit))
+                            if (!exclusions.ToList().Contains(post.Subreddit))
                             {
                                 post.Comment("#s");
+                                Console.WriteLine("posted a comment");
                             }
                         }
                     }
+                    catch (AggregateException e)
+                    {
+                        if(!e.ToString().Contains("Unable to find an album with the id,"))
+                        {
+                            goto retry;
+                        }
+                    }
+                    end:
                     Console.WriteLine("Adding ID so no reposts");
                     alreadySeen.Add(post.Id);
                     File.WriteAllLines("./seen.txt", alreadySeen);
-                    
+                    ;
                 }
             }
         }
